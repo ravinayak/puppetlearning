6fc3a78d2827832e9913dca2051f9e6303402f03
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-01-27 19:44:43 +0530

    Attributes of a file

---
+++ manifests/file_attrs.pp
@@ -0,0 +1,23 @@
+file { "/etc/ninja":
+	owner => 'ninja',
+	ensure => true,
+	group => 'ninja',
+	mode => '0644',
+}
+
+file { '/etc/sample_dir':
+	ensure => directory,
+	owner => 'vagrant',
+	group => 'vagrant',
+	mode => '0755',
+}
+
+file { '/etc/symbolic_link': 
+	ensure => link,
+	target => '/etc/motd',
+}
+
+file {'/etc/tree_sample_dir':
+	source => '/examples/sample_dir',
+	recure => true,
+}
