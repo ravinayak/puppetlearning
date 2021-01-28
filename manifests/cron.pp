7616af3ffc9d1f680afff07dbe6363c0fa730649
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-01-28 18:53:08 +0530

    Updating cron

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,7 +1,7 @@
 cron{ 'cron example':
 	command => '/bin/date +%F',
 	user => 'neo',
-	environment => ['MAILTO=ravinayak19@gmail.com', 'PATH:/bin'],
+	environment => ['MAILTO=ravinayak19@gmail.com', 'PATH=/bin'],
 	hour => '*',
 	minute => '*/15',
 	weekday => ['Thursday'], 
