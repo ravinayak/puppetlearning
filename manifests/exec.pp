750e4360c42954c1076b85b982bf059703f9cea0
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-01-28 19:25:03 +0530

    Exec puppet

---
+++ manifests/exec.pp
@@ -0,0 +1,13 @@
+file { '/tmp/file_creation':
+	ensure => directory,	
+}
+file { '/tmp/file_creation/file_creation.sh:'
+	ensure => file,
+	content => '#! /bin/bash
+	 touch abc.txt'
+}
+exec {'/tmp/file_creation.sh':
+	cwd => '/tmp/file_creation',
+	command => 'source file_creation.sh',
+	creates => '/tmp/file_creation/abc.txt',
+}
