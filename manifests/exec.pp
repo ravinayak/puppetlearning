6cd712adead0db1e6fcc44ea81aaf8f067761130
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-01-28 19:43:11 +0530

    Exec updated

--- manifests/exec.pp
+++ manifests/exec.pp
@@ -3,12 +3,12 @@ file { '/tmp/file_creation':
 }
 file { '/tmp/file_creation/file_creation.sh':
 	ensure => present,
-	content => 'touch abc.txt',
+	content => '/usr/bin/touch abc.txt',
 	mode => '0755'
 }
 exec {'random-command':
 	cwd => '/tmp/file_creation',
-	path => '/tmp/file_creation'
+	path => '/tmp/file_creation',
 	command => '/usr/bin/bash file_creation.sh',
 	creates => '/tmp/file_creation/abc.txt',
 }
