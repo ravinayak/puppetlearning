eb656136798db9030113d9ae766bcc14401a4f04
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-01-28 16:56:33 +0530

    Updating package puppet

--- manifests/package_puppet.pp
+++ manifests/package_puppet.pp
@@ -1,12 +1,8 @@
 group { 'dave':
  	gid => '20',
- 	ensure => present,
+ 	ensure => absent,
  } 
 
 user { 'new-user':
-	ensure => present,
-	uid => 1001,
-	home => '/home/new-user',
-	shell => '/bin/bash',
-	groups => ['dave'],
+	ensure => absent,
 }
