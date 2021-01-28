db9ab156f9196cffa1fce82e81be96deb89f36b7
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-01-28 16:09:57 +0530

    Updating package puppet

--- manifests/package_puppet.pp
+++ manifests/package_puppet.pp
@@ -11,5 +11,5 @@ service { 'ntp':
 	hasstatus => false,
 	pattern => 'ntpd',
 	hasrestart => true,
-	restart => '/bin echo Restarting >> /tmp/debug.log && systemctl restart ntp',
+	restart => '/bin echo Restarting >> /tmp/debug.log && sudo systemctl restart ntp',
 }
