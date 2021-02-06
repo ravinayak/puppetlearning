c6dc215d914f87d1d525abb8a15fc0844d4a8959
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-02-06 11:31:56 +0530

    Checking Changes in

---
+++ manifests/lookup2.pp
@@ -0,0 +1,6 @@
+notice("Apache is set to use $(lookup('apache_worker_factor', Integer)) workers")
+unless lookup('apparmor_enabled', Boolean) {
+	notice('I am in apparmor function')
+	exec('apt-get -y remove-apparmor')
+}
+notice('dns_allow_query_enabled: ', lookup('dns_allow_query', Boolean))
