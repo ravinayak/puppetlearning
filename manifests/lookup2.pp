4c637d945e3f955f05dbde63a85d93a685e839aa
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-02-06 11:34:54 +0530

    Checking Changes in

--- manifests/lookup2.pp
+++ manifests/lookup2.pp
@@ -1,4 +1,4 @@
-notice("Apache is set to use $(lookup('apache_worker_factor', Integer)) workers")
+notice("Apache is set to use ${lookup('apache_worker_factor', Integer)} workers")
 unless lookup('apparmor_enabled', Boolean) {
 	notice('I am in apparmor function')
 	exec('apt-get -y remove-apparmor')
