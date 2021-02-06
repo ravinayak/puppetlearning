167d9a53907fdd36faef387000e6777726c4f9ab
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-02-06 19:57:29 +0530

    Checking Changes in

--- manifests/lookup2.pp
+++ manifests/lookup2.pp
@@ -21,9 +21,12 @@ notice(lookup('duplicate_values', Array, 'unique'))
 
 lookup('users', Array[String]).each |String $user| {
 	notice("The value of user is ${user}")
-}
-lookup('users2', Hash, 'hash').each | String $user, Hash $attributes | {
-	user{ $user: 
-		* => $attributes
+	user { $user: 
+		ensure => absent,
 	}
 }
+# lookup('users2', Hash, 'hash').each | String $user, Hash $attributes | {
+# 	user{ $user: 
+# 		* => $attributes
+# 	}
+# }
