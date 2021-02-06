eb629a05d23cd99646bb61d1e760d8e8ddc34764
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-02-06 12:26:30 +0530

    Checking Changes in

--- manifests/lookup2.pp
+++ manifests/lookup2.pp
@@ -4,3 +4,7 @@ unless lookup('apparmor_enabled', Boolean) {
 	exec('apt-get -y remove-apparmor')
 }
 notice('dns_allow_query_enabled: ', lookup('dns_allow_query', Boolean))
+if lookup('is_production', Boolean) {
+	notice("Production environment")
+	notice("Here are the values of monitor_ips :: ${lookup('monitor_ips', Array[String])}")
+}
