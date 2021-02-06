f1026570745c52ea06378a2115092dfe2aab5163
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-02-06 20:02:13 +0530

    Checking Changes in

--- manifests/lookup2.pp
+++ manifests/lookup2.pp
@@ -21,12 +21,10 @@ notice(lookup('duplicate_values', Array, 'unique'))
 
 lookup('users', Array[String]).each |String $user| {
 	notice("The value of user is ${user}")
-	user { $user: 
-		ensure => absent,
+}
+notice("Use of literal in Hiera :: ${lookup('apache_http', String)}")
+lookup('users2', Hash, 'hash').each | String $user, Hash $attributes | {
+	user{ $user: 
+		* => $attributes
 	}
 }
-# lookup('users2', Hash, 'hash').each | String $user, Hash $attributes | {
-# 	user{ $user: 
-# 		* => $attributes
-# 	}
-# }
