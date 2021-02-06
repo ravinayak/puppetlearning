d5d8bd4383e2bb986884f340efa1cc9167f8b017
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-02-06 19:34:23 +0530

    Checking Changes in

--- manifests/lookup2.pp
+++ manifests/lookup2.pp
@@ -17,4 +17,8 @@ if(lookup('cobbler_config.pxe_just_once', Boolean)) {
 }
 notice("backup_path: ${lookup('backup_path', String)}")
 notice("new_vpn_allow_list: ${lookup('new_vpn_allow_list', Array[String])}")
-notice("${lookup('duplicate_values', Array, 'unique')}")
+notice(lookup('duplicate_values', Array, 'unique'))
+
+lookup('users', Array[String]).each |$user| {
+	notice("The value of user is ${user}")
+}
