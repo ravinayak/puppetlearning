80e5bd25051697eb9163e344f0b22cf65a9ab59d
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-07 12:39:15 +0000

    Changes

--- Puppetfile
+++ Puppetfile
@@ -1,7 +1,7 @@
 mod 'puppetlabs-stdlib', '8.1.0'
 mod 'basicresourcetypes',
 :git => 'git@github.com:ravinayak/basicresourcetypes',
-:tag => 'v6.121.0'
+:tag => 'v6.123.0'
 mod 'includecheck',
 :git => 'git@github.com:ravinayak/includecheck.git',
 :tag => 'v2.0.0'
--- manifests/tidyresourcetypedemo.pp
+++ manifests/tidyresourcetypedemo.pp
@@ -1,29 +1,34 @@
-include basicresourcetypes
-tidy { 'tidytmp1-dir':
-  path    => '/tmp/tidy-tmp-1',
-  matches => ['*.tmp*','*.pdf*','*.temp*', '*.txt*'],
-  age     => '1s',
-  type    => 'atime',
-  recurse => 1,
+unless find_file('/tmp/tidy-tmp-1/file.pdf0') {
+  include basicresourcetypes
+
 }
-tidy { 'tidytmp2-dir':
-  path      => '/tmp/tidy-tmp-2',
-  matches   => ['*.tmp*','*.pdf*'],
-  size      => '1k',
-  age       => '1s',
-  type      => 'atime',
-  max_files => 50,
-  recurse   => 1,
-}
-tidy { 'tidytmp3-dir':
-  path    => '/tmp/tidy-tmp-3',
-  matches => ['*.tmp*', '*.temp*', '*.txt*'],
-  size    => '1k',
-  recurse => 5,
-  rmdirs  => true,
-}
-tidy { 'tidytmp4-dir':
-  path    => '/tmp/tidy-tmp-4',
-  recurse => 1,
-  rmdirs  => true,
+if find_file('/tmp/tidy-tmp-1/file.pdf0') {
+  tidy { 'tidytmp1-dir':
+    path    => '/tmp/tidy-tmp-1',
+    matches => ['*.tmp*','*.pdf*','*.temp*', '*.txt*'],
+    age     => '1s',
+    type    => 'atime',
+    recurse => 1,
+  }
+  tidy { 'tidytmp2-dir':
+    path      => '/tmp/tidy-tmp-2',
+    matches   => ['*.tmp*','*.pdf*'],
+    size      => '1k',
+    age       => '1s',
+    type      => 'atime',
+    max_files => 50,
+    recurse   => 1,
+  }
+  tidy { 'tidytmp3-dir':
+    path    => '/tmp/tidy-tmp-3',
+    matches => ['*.tmp*', '*.temp*', '*.txt*'],
+    size    => '1k',
+    recurse => 5,
+    rmdirs  => true,
+  }
+  tidy { 'tidytmp4-dir':
+    path    => '/tmp/tidy-tmp-4',
+    recurse => 1,
+    rmdirs  => true,
+  }
 }
