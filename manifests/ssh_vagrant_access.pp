3cbfe3e38d0597b3cd0fc7c2e4ec9f2b47fabccd
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-12 17:42:52 +0530

    ssh vagrant access updated

--- manifests/ssh_vagrant_access.pp
+++ manifests/ssh_vagrant_access.pp
@@ -1,6 +1,6 @@
 ssh_authorized_key { 'vagrant@maverick':
   ensure => present,
   user   => 'vagrant',
-  type   => 'ssh-rsa',
-  key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABgQDQYsfS0+HZRGWWyXsF9UqSJPE8X98kES9yTYOvTvCVmos+M/+s7pB79VhfZXcLBfVu4CjVRSsjY0YRMI1xntbHkOaqIWTIM3L2ldbNyAGriP53AHS2Elbp9DPJnKD8pJGNxQPECRGM5jIMXBtRWzyZ39hMMYbuYTsMb5pOGn3FIB3Zj4ZVCentMuMlK+4HjPFzu3XXGqL3WsgSpcZUAwMQTr3xNhkR6/2UFKAEyWOoYDDXdW4vGhJyrmzoTrm7aNeydmmqqF372Ih6pgoHIfXVIxReSag0Y/+ekB4OlniK6GgTsKgjQl46Ay8QNDFB6U6i7hNlhsMFDnZAnSxv8C2scDqT+pRLZkJa3bj5eLQC4Wk8bztpBSClL8R7lCsz13GIujRu/8Lp385r5bWWrWarP5pos9XQ/Dl0b6lLEbJ6E2WQAx8Bv/Fx3Y5YByJDZUtUuOn6l9FvUi6jPFVazJiowKk4DI9dlNwvYlRi6MXEZkONIsi1y5YSlwSnxS5q1p8=',
+  type   => 'ecdsa-sha2-nistp256',
+  key    => 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJ0QjPCuJhYwXXyktZfArFsN3LxZKrayzahEe+nbhGG9JdMNbK4zAppOj/IQaG4SLIQjZIehIyu4bkVFQiPxTVE=',
 }
